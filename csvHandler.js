ec37af688adbc74a8fdf741e4c07fa47c9307940
peyton-lucas <camplucastein@gmail.com>
2019-10-14 13:33:36 -0500

    Debugging csv lambda handler to write S3 data to dyanmoDB

--- csvHandler.js
+++ csvHandler.js
@@ -5,6 +5,8 @@ import promiseCSV from "./promiseCSV";
 import { S3 } from "aws-sdk";
 
 export async function main(event, context) {
+  console.log(event);
+  console.log(context);
   const bucket = event['Records'][0]['s3']['bucket']['name'];
   const key = decodeURIComponent(event['Records'][0]['s3']['object']['key']);
   const s3Params = {
@@ -15,16 +17,37 @@ export async function main(event, context) {
   const s3 = new S3();
   const s3Stream = s3.getObject(s3Params).createReadStream();
   const mergedMetrics = await promiseCSV(s3Stream, {headers: true});
-  console.log("Before try / catch");
+  const userId = key.split('/')[1].split(':')[1];
+  console.log(userId);
+
+  // console.log(userId);
+  // console.log("Before DynamoDB stuff n things");
+  // let allDynamoParams = [];
+  // for (const earId in mergedMetrics) {
+  //   console.log("Inside for loop");
+  //   allDynamoParams.push({
+  //     TableName: process.env.tableName,
+  //     Item: {
+  //       userId: event['Records'][0]['userIdentity']['principalId'],
+  //       animalId: uuid.v1(),
+  //       earId: {
+  //         sire: mergedMetrics[earId].sire,
+  //         dam: mergedMetrics[earId].dam,
+  //         weights: mergedMetrics[earId].weight,
+  //         collectionTime: mergedMetrics[earId].collectionTime
+  //       }
+  //     }
+  //   });
+  // }
+  // console.log("After allDynamoParams");
+  // console.log("Before try / catch");
   try {
     console.log("Before DynamoDB stuff n things");
-    let allDynamoParams = [];
     for (const earId in mergedMetrics) {
-      console.log("After for loop");
-      allDynamoParams.push({
+      const record = {
         TableName: process.env.tableName,
         Item: {
-          userId: event.requestContext.identity.cognitoIdentityId,
+          userId: userId,
           animalId: uuid.v1(),
           earId: {
             sire: mergedMetrics[earId].sire,
@@ -33,25 +56,13 @@ export async function main(event, context) {
             collectionTime: mergedMetrics[earId].collectionTime
           }
         }
-      });
-      console.log("After allDynamoParams");
+      };
+      console.log(record);
+      await dynamoDbLib.call("put", record);
     }
     console.log("After DynamoDB stuff n things");
-      // allDynamoParams.push({
-      //   TableName: process.env.tableName,
-      //   Item: {
-      //     userId: event.requestContext.identity.cognitoIdentityId,
-      //     animalId: uuid.v1(),
-      //     earId: earId,
-      //     sire: mergedMetrics[earId].sire,
-      //     dam: mergedMetrics[earId].dam,
-      //     weights: mergedMetrics[earId].weight,
-      //     collectionTimes: mergedMetrics[earId].collectionTime
-      //   }
-      // });
-    await dynamoDbLib.call("put", allDynamoParams);
+    //await dynamoDbLib.call("put", allDynamoParams);
     return success({ status: true });
-    console.log("success!");
   } catch (e) {
     return failure({ status: false });
     console.log('failure :(');
