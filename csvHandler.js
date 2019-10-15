45c08920435a128c0e130c3e8f4990bd4a73cabe
peyton-lucas <camplucastein@gmail.com>
2019-10-15 11:12:45 -0500

    Integrating multiple lambda runtimes

--- csvHandler.js
+++ csvHandler.js
@@ -5,44 +5,17 @@ import promiseCSV from "./promiseCSV";
 import { S3 } from "aws-sdk";
 
 export async function main(event, context) {
-  console.log(event);
-  console.log(context);
   const bucket = event['Records'][0]['s3']['bucket']['name'];
   const key = decodeURIComponent(event['Records'][0]['s3']['object']['key']);
   const s3Params = {
     Bucket: bucket,
     Key: key
   };
-  console.log(s3Params);
   const s3 = new S3();
   const s3Stream = s3.getObject(s3Params).createReadStream();
   const mergedMetrics = await promiseCSV(s3Stream, {headers: true});
   const userId = key.split('/')[1].split(':')[1];
-  console.log(userId);
-
-  // console.log(userId);
-  // console.log("Before DynamoDB stuff n things");
-  // let allDynamoParams = [];
-  // for (const earId in mergedMetrics) {
-  //   console.log("Inside for loop");
-  //   allDynamoParams.push({
-  //     TableName: process.env.tableName,
-  //     Item: {
-  //       userId: event['Records'][0]['userIdentity']['principalId'],
-  //       animalId: uuid.v1(),
-  //       earId: {
-  //         sire: mergedMetrics[earId].sire,
-  //         dam: mergedMetrics[earId].dam,
-  //         weights: mergedMetrics[earId].weight,
-  //         collectionTime: mergedMetrics[earId].collectionTime
-  //       }
-  //     }
-  //   });
-  // }
-  // console.log("After allDynamoParams");
-  // console.log("Before try / catch");
   try {
-    console.log("Before DynamoDB stuff n things");
     for (const earId in mergedMetrics) {
       const record = {
         TableName: process.env.tableName,
@@ -57,15 +30,10 @@ export async function main(event, context) {
           }
         }
       };
-      console.log(record);
       await dynamoDbLib.call("put", record);
     }
-    console.log("After DynamoDB stuff n things");
-    //await dynamoDbLib.call("put", allDynamoParams);
     return success({ status: true });
   } catch (e) {
     return failure({ status: false });
-    console.log('failure :(');
   }
-  console.log("After try / catch");
 }
--- livestockAnalytics.py
+++ livestockAnalytics.py
@@ -2,6 +2,7 @@ import json
 import numpy as np
 import pandas as pd
 import boto3
+import urllib.parse
 import sys
 from datetime import datetime
 
@@ -16,38 +17,50 @@ def averageDailyGain(event, context):
       for each interval in benchmarkInterval
         print(interval + " weight is " + livestockFunction(interval))
     """
-
+    print(event)
+    print(context)
+    # Lambda event is a get request from dashboard container
     db  = boto3.resource('dynamodb')
-    table = db-Table('userId')
+    table = db.Table('dev-farms')
+    # s3Key = urllib.parse.unquote(event['Records'][0]['s3']['object']['key'])
+    # userId = s3Key.split('/')[1].split(':')[1]
+    userId = event.context.identity.cognito_identity_id
     # Retrieves user livestock data
     response = table.get_item(
         {
             # userIdVal is event.requestContext.identity.cognitoIdentityId)
-            'userId': event.requestContext.identity.cognitoIdentityId
+            'userId': userId
         }
     )
-
-    ids = np.unique(points[:,0])
-    day60Weights = []
-    day90Weights = []
-    day120Weights = []
-    for id in ids:
+    print(response)
+    return {
+        'statusCode': 200,
+        'body': response
+    }
+    # ids = np.unique(points[:,0])
+    # day60Weights = []
+    # day90Weights = []
+    # day120Weights = []
+    # for id in ids:
         # print('id: ' + str(id))
-        metrics = points[np.where(points[:, 0] == id)[0]]
+        # metrics = points[np.where(points[:, 0] == id)[0]]
 
-        if metrics.size > 3:
-            x = metrics[:, 1] - metrics[0, 1]
-            y = metrics[:, 2]
-            livestockFit = np.polyfit(np.float32(x), np.float32(y), 1)
-            livestockFunction = np.poly1d(livestockFit)
+        # if metrics.size > 3:
+            # x = metrics[:, 1] - metrics[0, 1]
+            # y = metrics[:, 2]
+            # livestockFit = np.polyfit(np.float32(x), np.float32(y), 1)
+            # livestockFunction = np.poly1d(livestockFit)
 
-            day60Weights.append(livestockFunction(60*86400))
-            day90Weights.append(livestockFunction(90*86400))
-            day120Weights.append(livestockFunction(120*86400))
+            # day60Weights.append(livestockFunction(60*86400))
+            # day90Weights.append(livestockFunction(90*86400))
+            # day120Weights.append(livestockFunction(120*86400))
 
     # print(np.asarray(day60Weights), np.asarray(day90Weights), np.asarray(day120Weights))
-    return json.dumps({
-        'day60Weights': day60Weights,
-        'day90Weights': day90Weights,
-        'day120Weights': day120Weights
-    })
+    # return json.dumps({
+        # 'day60Weights': day60Weights,
+        # 'day90Weights': day90Weights,
+        # 'day120Weights': day120Weights
+    # })
+
+if __name__ == "__livestock-analytics__":
+    livestock-analytics('', '')
